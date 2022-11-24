f548976e46be1610d8afdb8594195a854a52dd76
“Viktor <viktorekby@gmail.com>
2022-11-24 12:54:10 +0100

    add sqlite

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use rusqlite::{params, Connection, Result};
+
+#[derive(Debug)]
+struct Password {
+    site: String,
+    password: String,
+}
+
+fn main() -> Result<(), rusqlite::Error> {
+    let conn = Connection::open_in_memory()?;
+
+    conn.execute(
+        "CREATE TABLE password(
+            id          INTEGER PRIMARY KEY,
+            site        TEXT NOT NULL,
+            password    TEXT NOT NULL
+        )",
+        (),
+    )?;
+
+    let site = Password {
+        site: "google.com".to_string(),
+        password: "password".to_string(),
+    };
+
+    conn.execute(
+        "INSERT INTO password (site, password) VALUES (?1, ?2)",
+        (&site.site, &site.password),
+    )?;
+
+    let mut stmt = conn.prepare("SELECT site, password FROM password")?;
+    let person_iter = stmt.query_map([], |row| {
+        Ok(Password {
+            site: row.get(0)?,
+            password: row.get(1)?,
+        })
+    })?;
+
+    for person in person_iter {
+        println!("Found person {:?}", person.unwrap());
+    }
+    Ok(())
 }
